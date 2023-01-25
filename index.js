2263562fbd3a11c63b313c3538bf02b19d24fddd
Nora Söderlund <norasoderlund@icloud.com>
2023-01-26 00:46:17 +0100

    Fixed html urls

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ const notifications = await octokit.request("GET /notifications", {
 for(let index = 0; index < notifications.data.length; index++) {
     const notification = notifications.data[index];
 
+    let subject_url = null;
+
+    if(!notification.repository.private) {
+        const thread = await octokit.request("GET " + notification.subject.url);
+
+        subject_url = thread.data.html_url;
+    }
+
     items.push({
         id: notification.id,
         reason: notification.reason,
@@ -33,14 +41,15 @@ for(let index = 0; index < notifications.data.length; index++) {
         
         subject: {
             title: notification.subject.title,
-            url: notification.subject.url,
+            url: subject_url,
             type: notification.subject.type,
         },
 
         repository: {
-            name: notification.repository.full_name,
+            name: notification.repository.name,
+            full_name: notification.repository.full_name,
             description: notification.repository.description,
-            url: notification.repository.url,
+            url: (notification.repository.private)?(null):(notification.repository.html_url),
 
             owner: {
                 avatar: notification.repository.owner.avatar_url
@@ -56,14 +65,14 @@ await new Promise((resolve) => {
 
         await Promise.all(items.map((item) => {
             return new Promise((resolveQuery) => {
-                connection.query(`SELECT * FROM github WHERE id = ${connection.escape(item.id)}`, (error, rows) => {
+                connection.query(`SELECT * FROM github WHERE id = ${connection.escape(item.id)}`, async (error, rows) => {
                     if(error)
                         throw error;
 
                     if(rows.length)
                         return resolveQuery();
 
-                    connection.query(`INSERT INTO github (id, reason, updated_at, subject_title, subject_url, subject_type, repository_name, repository_description, repository_url, repository_owner_avatar) VALUES (${connection.escape(item.id)}, ${connection.escape(item.reason)}, ${connection.escape(item.updated_at)}, ${connection.escape(item.subject.title)}, ${connection.escape(item.subject.url)}, ${connection.escape(item.subject.type)}, ${connection.escape(item.repository.name)}, ${connection.escape(item.repository.description)}, ${connection.escape(item.repository.url)}, ${connection.escape(item.repository.owner.avatar)})`, (error) => {
+                    connection.query(`INSERT INTO github (id, reason, updated_at, subject_title, subject_url, subject_type, repository_name, repository_full_name, repository_description, repository_url, repository_owner_avatar) VALUES (${connection.escape(item.id)}, ${connection.escape(item.reason)}, ${connection.escape(item.updated_at)}, ${connection.escape(item.subject.title)}, ${connection.escape(item.subject.url)}, ${connection.escape(item.subject.type)}, ${connection.escape(item.repository.name)}, ${connection.escape(item.repository.full_name)}, ${connection.escape(item.repository.description)}, ${connection.escape(item.repository.url)}, ${connection.escape(item.repository.owner.avatar)})`, (error) => {
                         if(error)
                             throw error;
 
